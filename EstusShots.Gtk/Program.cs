b350cee2bcd5b04df5da5f545191ac59a2b6944a
Marcel Fries <m.fries@eas.domain.local>
2020-03-16 14:40:56 +0100

    Use custom gtk theme when compiling and running on widows

--- EstusShots.Gtk/Program.cs
+++ EstusShots.Gtk/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using Gtk;
 
 namespace EstusShots.Gtk
@@ -10,6 +11,17 @@ namespace EstusShots.Gtk
         {
             Application.Init();
 
+            // Load a custom theme only when running on windows
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var provider = new CssProvider();
+                provider.LoadFromPath("Theme/gtk-3.20/gtk.css");
+                StyleContext.AddProviderForScreen(Gdk.Screen.Default, provider, 800);
+                
+                // TODO ????
+                //IconTheme.Default.PrependSearchPath("Theme/icons/48x48");
+            }
+            
             var app = new Application("EstusShots.Gtk", GLib.ApplicationFlags.None);
             app.Register(GLib.Cancellable.Current);
 
